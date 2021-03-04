d847da4e55382f3f10dd6c1260df83036a8cc87d
alexgpitts <alexgpitts@gmail.com>
2021-03-03 20:10:11 -0800

    Wrote first test
    Checks years evenly divisible by 4, but not 100 or 400.

---
+++ test_leap_year.py
@@ -0,0 +1,17 @@
+import unittest
+import leap_year
+
+
+#unittest class tests
+class TestCase(unittest.TestCase):
+
+    # A test that checks 2004 which is a leap year evenly 
+    # divisible by 4, but not 100 and not 400
+    def test_leap_year_1(self): 
+        self.assertEqual(leap_year.is_leap_year(2004), True) 
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
